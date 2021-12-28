ea0dd7151a8925d5f1b31811b52a3be90676c2e9
Brian Shimkus <brianshimkus1@gmail.com>
2021-12-27 23:22:40 -0600

    Set up user registration

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,11 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import {
+	getAuth,
+	createUserWithEmailAndPassword,
+	updateProfile,
+} from 'firebase/auth'
+import { db } from '../firebase.config'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -22,6 +28,30 @@ export default function SignUp() {
 		}))
 	}
 
+	const onSubmit = async (e) => {
+		e.preventDefault()
+
+		try {
+			const auth = getAuth()
+
+			const userCredential = await createUserWithEmailAndPassword(
+				auth,
+				email,
+				password
+			)
+
+			const user = userCredential.user
+
+			updateProfile(auth.currentUser, {
+				displayName: name,
+			})
+
+			navigate('/')
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
 	return (
 		<>
 			<div className='pageContainer'>
@@ -29,13 +59,13 @@ export default function SignUp() {
 					<p className='pageHeader'>Sign Up</p>
 				</header>
 
-				<form>
+				<form onSubmit={onSubmit}>
 					<input
 						type='text'
 						className='nameInput'
 						placeholder='Name'
 						id='name'
-						value={email}
+						value={name}
 						onChange={onChange}
 					/>
 
