c1631aff0311511a523f4aae8a7883a97b68026c
Brian Shimkus <brianshimkus1@gmail.com>
2021-12-29 14:42:43 -0600

    Built out Offers page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ export default function Navbar() {
 									? 'navbarListItemNameActive'
 									: 'navbarListItemName'
 							}>
-							Offer
+							Offers
 						</p>
 					</li>
 					<li className='navbarListItem' onClick={() => navigate('/profile')}>
