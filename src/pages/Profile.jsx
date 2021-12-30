0524c8c4db0542b95b80b6eb80c1f21e2a990f82
Brian Shimkus <brianshimkus1@gmail.com>
2021-12-29 22:43:47 -0600

    Started building the Create Listing page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 import { getAuth, updateProfile } from 'firebase/auth'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { doc, updateDoc } from 'firebase/firestore'
 import { db } from '../firebase.config'
 import { toast } from 'react-toastify'
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
+import homeIcon from '../assets/svg/homeIcon.svg'
 
 export default function Profile() {
 	const auth = getAuth()
@@ -88,6 +90,12 @@ export default function Profile() {
 						/>
 					</form>
 				</div>
+
+				<Link to='/create-listing' className='createListing'>
+					<img src={homeIcon} alt='home' />
+					<p>Sell or rent your home</p>
+					<img src={arrowRight} alt='arrow' />
+				</Link>
 			</main>
 		</div>
 	)
