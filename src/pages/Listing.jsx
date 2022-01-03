985ccdf6df8329800d7976a31dac05905f689283
Brian Shimkus <brianshimkus1@gmail.com>
2022-01-02 23:54:30 -0600

    Added Listings page slider

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,16 +1,21 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
+import { Helmet } from 'react-helmet'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/swiper-bundle.css'
 import { getDoc, doc } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 import { db } from '../firebase.config'
 import Spinner from '../components/Spinner'
 import shareIcon from '../assets/svg/shareIcon.svg'
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
 export default function Listing() {
 	const [listing, setListing] = useState(null)
 	const [loading, setLoading] = useState(true)
-	const [shareLinkCopied, setShareLinkCopied] = useState(null)
+	const [shareLinkCopied, setShareLinkCopied] = useState(false)
 
 	const navigate = useNavigate()
 	const params = useParams()
@@ -21,7 +26,7 @@ export default function Listing() {
 			const docRef = doc(db, 'listings', params.listingId)
 			const docSnap = await getDoc(docRef)
 
-			if (docSnap.exists) {
+			if (docSnap.exists()) {
 				setListing(docSnap.data())
 				setLoading(false)
 			}
@@ -36,6 +41,22 @@ export default function Listing() {
 
 	return (
 		<main>
+			<Helmet>
+				<title>{listing.name}</title>
+			</Helmet>
+			<Swiper slidesPerView={1} pagination={{ clickable: true }}>
+				{listing.imgUrls.map((url, index) => (
+					<SwiperSlide key={index}>
+						<div
+							style={{
+								background: `url(${listing.imgUrls[index]}) center no-repeat`,
+								backgroundSize: 'cover',
+							}}
+							className='swiperSlideDiv'></div>
+					</SwiperSlide>
+				))}
+			</Swiper>
+
 			<div
 				className='shareIconDiv'
 				onClick={() => {
@@ -45,12 +66,10 @@ export default function Listing() {
 						setShareLinkCopied(false)
 					}, 2000)
 				}}>
-				<img src={shareIcon} alt='share' />
+				<img src={shareIcon} alt='' />
 			</div>
 
-			{shareLinkCopied && (
-				<p className='shareLinkCopied'>Link copied to clipboard</p>
-			)}
+			{shareLinkCopied && <p className='linkCopied'>Link Copied!</p>}
 
 			<div className='listingDetails'>
 				<p className='listingName'>
@@ -77,12 +96,12 @@ export default function Listing() {
 					<li>
 						{listing.bedrooms > 1
 							? `${listing.bedrooms} Bedrooms`
-							: '1 bedroom'}
+							: '1 Bedroom'}
 					</li>
 					<li>
 						{listing.bathrooms > 1
 							? `${listing.bathrooms} Bathrooms`
-							: '1 bedroom'}
+							: '1 Bathroom'}
 					</li>
 					<li>{listing.parking && 'Parking Spot'}</li>
 					<li>{listing.furnished && 'Furnished'}</li>
@@ -95,7 +114,7 @@ export default function Listing() {
 						style={{ height: '100%', width: '100%' }}
 						center={[listing.geolocation.lat, listing.geolocation.lng]}
 						zoom={13}
-						scrollWheelZoom={true}>
+						scrollWheelZoom={false}>
 						<TileLayer
 							attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 							url='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
@@ -108,9 +127,9 @@ export default function Listing() {
 					</MapContainer>
 				</div>
 
-				{auth.currentUser?.uid === !listing.userRef && (
+				{auth.currentUser?.uid !== listing.userRef && (
 					<Link
-						to={`/chat/${listing.userRef}?listingName=${listing.name}`}
+						to={`/contact/${listing.userRef}?listingName=${listing.name}`}
 						className='primaryButton'>
 						Contact Landlord
 					</Link>
