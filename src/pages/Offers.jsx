066eb3905ed9f98dfd0e4e9863b2f2327b7f5dd5
Brian Shimkus <brianshimkus1@gmail.com>
2022-01-03 23:25:22 -0600

    Set up load more pagination on Offers page

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 import {
 	collection,
 	getDocs,
@@ -14,11 +13,10 @@ import { toast } from 'react-toastify'
 import Spinner from '../components/Spinner'
 import ListingItem from '../components/ListingItem'
 
-export default function Offers() {
+function Offers() {
 	const [listings, setListings] = useState(null)
 	const [loading, setLoading] = useState(true)
-
-	const params = useParams()
+	const [lastFetchedListing, setLastFetchedListing] = useState(null)
 
 	useEffect(() => {
 		const fetchListings = async () => {
@@ -26,7 +24,7 @@ export default function Offers() {
 				// Get reference
 				const listingsRef = collection(db, 'listings')
 
-				// Query
+				// Create a query
 				const q = query(
 					listingsRef,
 					where('offer', '==', true),
@@ -36,6 +34,9 @@ export default function Offers() {
 
 				// Execute query
 				const querySnap = await getDocs(q)
+
+				const lastVisible = querySnap.docs[querySnap.docs.length - 1]
+				setLastFetchedListing(lastVisible)
 
 				const listings = []
 
@@ -56,11 +57,49 @@ export default function Offers() {
 		fetchListings()
 	}, [])
 
+	// Pagination / Load More
+	const onFetchMoreListings = async () => {
+		try {
+			// Get reference
+			const listingsRef = collection(db, 'listings')
+
+			// Create a query
+			const q = query(
+				listingsRef,
+				where('offer', '==', true),
+				orderBy('timestamp', 'desc'),
+				startAfter(lastFetchedListing),
+				limit(10)
+			)
+
+			// Execute query
+			const querySnap = await getDocs(q)
+
+			const lastVisible = querySnap.docs[querySnap.docs.length - 1]
+			setLastFetchedListing(lastVisible)
+
+			const listings = []
+
+			querySnap.forEach((doc) => {
+				return listings.push({
+					id: doc.id,
+					data: doc.data(),
+				})
+			})
+
+			setListings((prevState) => [...prevState, ...listings])
+			setLoading(false)
+		} catch (error) {
+			toast.error('Could not fetch listings')
+		}
+	}
+
 	return (
 		<div className='category'>
 			<header>
 				<p className='pageHeader'>Offers</p>
 			</header>
+
 			{loading ? (
 				<Spinner />
 			) : listings && listings.length > 0 ? (
@@ -76,6 +115,14 @@ export default function Offers() {
 							))}
 						</ul>
 					</main>
+
+					<br />
+					<br />
+					{lastFetchedListing && (
+						<p className='loadMore' onClick={onFetchMoreListings}>
+							Load More
+						</p>
+					)}
 				</>
 			) : (
 				<p>There are no current offers</p>
@@ -83,3 +130,5 @@ export default function Offers() {
 		</div>
 	)
 }
+
+export default Offers
